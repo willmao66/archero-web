3f73c3d841b28fc45ee4bcef03928c96098f4112
willmao66 <willmao@m3lab.io>
2024-06-17 19:53:09 +0800

    #

--- main.js
+++
@@ -1,6 +0,0 @@
-import TonConnectUI from '@tonconnect/ui';
-
-const tonConnectUI = new TonConnectUI({ //连接应用
-    manifestUrl: 'https://archero.ttgames.xyz/manifest/arhero.json',
-    buttonRootId: 'connectButton'
-});
